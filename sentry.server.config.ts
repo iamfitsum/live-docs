32ef146396a673ebae1627dd47f087c8f32086fe
iamfitsum <official.fitsum.mekonnen@gmail.com>
2025-01-19 23:56:34 +0300

    Added sentry and mention functionality in comments

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://dbcb354bfd4e03b887b2cc9ebeaaa31c@o4508672147587072.ingest.de.sentry.io/4508672152961104",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
